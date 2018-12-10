0c0cbe01fabfac7532b2e7d0b78b855047eac529
Steven Adams <steven@adaptivelite.com>
2018-12-09 22:25:26 -0700

    Finished social messages.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //DO NOT COMMIT CODE HERE
 const config = require( "./config" );
+const quotes = require( "./things-to-say" );
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -34,6 +35,28 @@ class TGCBot{
     
   }
 
+  SaySnappyQuote(){
+    let openers = Object.keys( quotes.statements );
+    let openerIndex = Math.floor(Math.random() * openers.length );
+    let opener = openers[openerIndex];
+    let swiches = Object.keys( quotes.statements[opener] );
+    let selectedSwitch = swiches[Math.floor(Math.random() * swiches.length )];
+    let enders = quotes.statements[opener][selectedSwitch];
+    let selectedEnder = enders[Math.floor(Math.random() * enders.length )];
+    let fullQuote = `${opener} ${selectedSwitch} ${selectedEnder}`;
+
+    let matches = fullQuote.match( /\{([A-z]+)\}/gi );
+    if( matches !== null ){
+      matches.forEach( x => {
+        let matchName = x.substr( 1, x.length - 2 );
+        fullQuote = fullQuote.replace( x, quotes[matchName][Math.floor(Math.random() * quotes[matchName].length )] )
+      } );
+    }
+
+    return fullQuote;
+    
+  }
+
   _SetupEvents(){
     this._OnNewMember();
     this._OnByeMember();
@@ -47,11 +70,11 @@ class TGCBot{
   }
 
   _OnNewMember(){
-    //const newMemberSnapy = 
+    const newMemberSnapy = this.SaySnappyQuote();
     this.client.on( "guildMemberAdd", member => {
       if( this.welcomeList.filter( x => x === member.user.id ).length === 0 ){
         this.welcomeList.push( member.user.id );
-        this.WriteMessage( "social-lobby", `Welcome <@${member.user.id}>! I have even more cookies!` );
+        this.WriteMessage( "social-lobby", `Welcome <@${member.user.id}>! ${newMemberSnapy}` );
         setTimeout( () => this.welcomeList.splice( this.welcomeList.indexOf( member.user.id ), 1 ), 30000 );
       }
     } );
