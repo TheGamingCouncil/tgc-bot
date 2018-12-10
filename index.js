b14c8e63dd09f871c26760ef1be54a58ea3b09fa
Steven Adams <steven@adaptivelite.com>
2018-12-09 21:19:20 -0700

    Minor corrects and additional things to say, as well as the command system.

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ client.login( config.token );
 class TGCBot{
   constructor( client ){
     this.client = client;
+    this.events = {};
+    this.welcomeList = [];
     this._SetupEvents();
     
   }
@@ -35,38 +37,91 @@ class TGCBot{
   _SetupEvents(){
     this._OnNewMember();
     this._OnByeMember();
+
+    this._ListenForEvent( [ "*" ], "help", ( user, message ) => {
+      this._ServerReply( user, `At this point my help is weak as the only supported command is help. So I really can't help you.`  );
+    } );
+
+    this._SetupMessageListeners();
+    //this._ListenForEvent( [ "*" ], "event" );
   }
 
   _OnNewMember(){
+    //const newMemberSnapy = 
     this.client.on( "guildMemberAdd", member => {
-      this.WriteMessage( "social-lobby", `Welcome <@${member.user.id}>! I have even more cookies!` );
+      if( this.welcomeList.filter( x => x === member.user.id ).length === 0 ){
+        this.welcomeList.push( member.user.id );
+        this.WriteMessage( "social-lobby", `Welcome <@${member.user.id}>! I have even more cookies!` );
+        setTimeout( () => this.welcomeList.splice( this.welcomeList.indexOf( member.user.id ), 1 ), 30000 );
+      }
     } );
   }
 
   _OnByeMember(){
     this.client.on( "guildMemberRemove", member => {
-      this.WriteMessage( "social-lobby", `A member has left our ranks! Let us all sing a song and remember the good times we had with <@${member.user.id}>!` );
+      const aka = `AKA(${member.nickname || member.username})`;
+      this.WriteMessage( "social-lobby", `A member has left our ranks! Let us all sing a song and remember the good times we had with ${member.user.tag} ${aka}!` );
     } );
   }
 
-  GetChannelByName( name ){
-    return this.client.channels.filter( x => x.name === name ).array()[0] || null;
+  GetChannelsByName( name ){
+    return this.client.channels.filter( x => x.name === name ).array();
   }
 
   GetUserByName( name ){
-    return this.client.users.filter( x => x.username === name ).array()[0] || null;
+    return this.client.users.filter( x => x.username === name ).array();
   }
 
   WriteMessage( channel, message ){
     if( typeof( channel ) === "string" ){
-      channel = this.GetChannelByName( channel );
+      channel = this.GetChannelsByName( channel );
     }
-    if( channel !== null ){
-      channel.send( message );
-    }
-    else{
-      //console.log( channel );
-    }
+
+    channel.forEach( x => x.send( message ) );
+  }
+
+  UserHasPermission( user, roleName ){
+
+  }
+
+  _ListenForEvent( supportedChannels, command, method ){
+    this.events[command] = {
+      supportedChannels,
+      method
+    };
+  }
+
+  _SetupMessageListeners(){
+    this.client.on( "message", async ( message ) => {
+      if( message.content.startsWith( "tgc" ) ){
+        await message.delete();
+        let messageBody = message.content.substring( 4 ).split( " " );
+        let messageCommand = messageBody[0] || "unknown";
+        messageBody.splice( 0, 1 );
+        if( this.events[messageCommand.toLowerCase()] ){
+          const command = this.events[messageCommand.toLowerCase()];
+          if( command.supportedChannels.filter( x => x === "*" || x === message.channel.name ).length > 0 ){
+            command.method.apply( this, [ message.author, message, ...messageBody ] );
+          }
+          else{
+            this._ServerReply( message.author, `Invalid channel for command issued '${messageCommand}':'${message.channel.name}'`  );
+          }
+        }
+        else{
+          this._ServerReply( message.author, `Invalid command issued '${messageCommand}'` );
+        }
+        
+      }
+    });
+  }
+
+  async _ServerReply( user, message, decayTimer = 30000 ){
+    let reply = await user.send( "```" + message + ".```\n\n```This message will self destruct in " + ( decayTimer / 1000 ) + " seconds.```" );
+    setTimeout( () => reply.delete(), decayTimer );
+  }
+
+  IsListeningChannel(){
+
   }
 
 }
