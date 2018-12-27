249cd00d8a044a47b5be948a6e87f409548221b8
Steven Adams <steven@adaptivelite.com>
2018-12-26 19:44:15 -0700

    Added uberWelcome command

--- commands/help.js
+++ commands/help.js
@@ -9,5 +9,14 @@ module.exports = class Help extends Command{
   async help( command ){
     command.ServerReply( `At this point my help is weak as the only supported command is help. So I really can't help you.`  );
   }
+
+  async uberWelcome( command ){
+    
+    let user = command.message.mentions.users.array()[0] || null;
+    if( user !== null ){
+      command.message.channel.send( `${command.user.id} welcomes ${user.id}`)
+    }
+    
+  }
   
 };
